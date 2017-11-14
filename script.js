302f661485e22d3f02b1c3e94e9864bddc0a47d9
Manuel Pichardo <mlpichardo1@gmail.com>
2017-11-14 01:59:20 +0000

    Merge

---
+++ script.js
@@ -0,0 +1,15 @@
+var todos = ['item 1', 'item 2', 'item 3']
+
+console.log('hello there, Manny')
+
+console.log('My Todos:' ,todos)
+
+todos.push('item4')
+
+todos.push('item5')
+
+todos[0]
+
+
+
+todos.splice(0, 1)
