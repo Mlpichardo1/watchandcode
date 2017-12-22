c4e016a4fb378dd875aeadca7ebf0688f51033e7
Manuel Pichardo <mlpichardo1@gmail.com>
2017-12-22 22:16:53 +0000

    Added Boolean Function

--- lesson2.js
+++ lesson2.js
@@ -7,3 +7,20 @@ var todos = ['item 1', 'item 2', 'item 3'];
 function displayTodos() {
     console.log('My Todos:', todos);
 }
+
+function addTodo(todo) {
+    todos.push(todo);
+    displayTodos();
+}
+
+function changeTodo(position, newValue) {
+    todos[position] = newValue; 
+}
+
+function deleteTodo(position) {
+    todos.splice(position, 1);
+    displayTodos();
+}
+
+addTodo('Some Stuff');
+displayTodos();
